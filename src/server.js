4d5248dad1f6d6526df0f32c79a4e568abc7c5f8
yuristmagistr <144477228+yuristmagistr@users.noreply.github.com>
2024-06-21 18:45:40 +0200

    update server

--- src/server.js
+++ src/server.js
@@ -18,6 +18,12 @@ export const setupServer = () => {
     }),
   );
 
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
+  });
+
   app.use(cors());
 
   app.use(express.json());
--- src/utils/calculatePaginationData.js
+++ src/utils/calculatePaginationData.js
@@ -8,7 +8,7 @@ export const calculatePaginationData = (count, perPage, page) => {
     perPage,
     totalItems: count,
     totalPages,
-    hasNextPage,
     hasPreviousPage,
+    hasNextPage,
   };
 };
